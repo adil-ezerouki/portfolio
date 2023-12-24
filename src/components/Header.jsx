549f2979f1785f4f7a99b30e0676b6dd0c8e8429
Adil Ezerouki <adil.ezerouki@gmail.com>
2023-12-24 23:48:55 +0100

    vercel test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,12 +6,12 @@ export default function Header() {
   const [isOpen, setOpen] = useState(false)
   
   return (
-    <header className='w-full h-20 flex justify-between items-center shadow-md '>
-      <div id='logo' className='md:pl-10 pl-5'>
+    <header className='w-full h-20 flex justify-between items-center shadow-lg '>
+      <div id='logo' className='lg:pl-10 pl-5'>
         <h1>LOGO</h1>
       </div>
-      <nav className={`md:shadow-none shadow-md md:pr-10 flex justify-center items-center p-6 md:bg-white bg-gray-200 md:static absolute md:w-auto md:z-auto z-[1] w-full  transition-all duration-500 ease-in top-[83px] ${isOpen ? 'left-0 ' : 'md:left-0 left-[-790px]'}`}>
-        <ul className='flex md:flex-row flex-col justify-center items-center gap-10'>
+      <nav className={`lg:shadow-none shadow-lg lg:pr-10 flex justify-center items-center p-6 lg:bg-white bg-gray-200 lg:static absolute lg:w-auto lg:z-auto z-[1] w-full  transition-all duration-500 ease-in top-[83px] ${isOpen ? 'left-0 ' : 'lg:left-0 left-[-790px]'}`}>
+        <ul className='flex lg:flex-row flex-col justify-center items-center gap-10'>
           {
             navLinks.map((navlink) => (
               <li key={navlink.name}><a href='#'>{navlink.name}</a></li>
@@ -21,7 +21,7 @@ export default function Header() {
         </ul>
       </nav>
 
-      <div className='md:hidden block pr-3'>
+      <div className='lg:hidden block pr-3'>
         <Hamburger onToggle={() => {
           setOpen(!isOpen)
         }} />
@@ -29,6 +29,3 @@ export default function Header() {
     </header>
   )
 }
-
-
-// ${isOpen ? 'top-20 ':'top-[-490px]'}
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,18 +3,18 @@ import img from '../assets/adil1.png'
 
 export default function Hero() {
   return (
-    <div id='heroSection' className=''>
-      <div className='flex md:flex-row flex-col-reverse justify-between items-center'>
-        <div id='leftPart' className=' md:w-[50%] md:text-left text-center flex flex-col gap-4 md:items-start items-center'>
+    <div id='heroSection' className='bg-yellow-200'>
+      <div className='flex lg:flex-row flex-col-reverse justify-between items-center'>
+        <div id='leftPart' className=' lg:w-[50%] lg:text-left text-center flex flex-col gap-4 lg:items-start items-center'>
           <div className='flex gap-2'>
             <div className='bg-[#177771] w-20 h-3 rounded-lg'></div>
             <div className='bg-[#FF7F40] w-8 h-3 rounded-lg'></div>
           </div>
           <div id='nameANDprofession'>
-            <h1 className='md:text-5xl text-4xl'>
+            <h1 className='lg:text-5xl text-4xl'>
               I'm Adil Ezerouki
             </h1>
-            <h1 className='md:text-5xl text-4xl'>
+            <h1 className='lg:text-5xl text-4xl'>
               Ful-Stack Web Developer
             </h1>
           </div>
@@ -23,7 +23,7 @@ export default function Hero() {
             incidunt consequatur aspernatur aperiam molestiae autem rem
             corporis deleniti, ea facere consectetur blanditiis obcaecati
             nesciunt harum?</p>
-          <div id='btns' className='flex md:flex-row flex-col gap-4'>
+          <div id='btns' className='flex lg:flex-row flex-col gap-4'>
             <button className='bg-[#177771] text-white px-6 py-3 rounded-full' type='button'>DOWNLOAD CV</button>
             <button className='border border-[#177771] text-[#177771] px-6 py-3 rounded-full' type='button'>VIEW PORTFOLIO</button>
           </div>
