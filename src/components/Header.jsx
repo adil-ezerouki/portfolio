8160f0f67e88ca9f58e7450579006dcf104bc394
Adil Ezerouki <adil.ezerouki@gmail.com>
2023-12-24 23:33:34 +0100

    responsive hero section is done

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,25 +1,28 @@
 import React, { useState } from 'react'
 import { Squash as Hamburger } from 'hamburger-react'
+import navLinks from '../data/navLinks.json'
 
 export default function Header() {
   const [isOpen, setOpen] = useState(false)
+  
   return (
-    <header className='bg-black w-full h-20 text-white flex justify-between items-center'>
+    <header className='w-full h-20 flex justify-between items-center shadow-md '>
       <div id='logo' className='md:pl-10 pl-5'>
         <h1>LOGO</h1>
       </div>
-      <nav className={`md:pr-10 flex justify-center items-center p-6 bg-yellow-400 md:static absolute md:w-auto md:z-auto z-[1] w-full  transition-all duration-500 ease-in top-20 ${isOpen ? 'left-0 ':'md:left-0 left-[-790px]'}`}>
+      <nav className={`md:shadow-none shadow-md md:pr-10 flex justify-center items-center p-6 md:bg-white bg-gray-200 md:static absolute md:w-auto md:z-auto z-[1] w-full  transition-all duration-500 ease-in top-[83px] ${isOpen ? 'left-0 ' : 'md:left-0 left-[-790px]'}`}>
         <ul className='flex md:flex-row flex-col justify-center items-center gap-10'>
-          <li><a href="">Home</a></li>
-          <li><a href="">About me</a></li>
-          <li><a href="">services</a></li>
-          <li><a href="">portfolio</a></li>
-          <li><a href="">contact</a></li>
+          {
+            navLinks.map((navlink) => (
+              <li key={navlink.name}><a href='#'>{navlink.name}</a></li>
+            ))
+          }
+          <li><a className='border border-[#177771] text-[#177771] px-4 py-2 rounded-full' href="#">contact</a></li>
         </ul>
       </nav>
 
       <div className='md:hidden block pr-3'>
-        <Hamburger onToggle={toggled => {
+        <Hamburger onToggle={() => {
           setOpen(!isOpen)
         }} />
       </div>
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,11 +1,37 @@
 import React from 'react'
+import img from '../assets/adil1.png'
 
 export default function Hero() {
   return (
-    <div id='heroSection' className='bg-slate-600'>
-        <div className='bg-amber-400 h-[83.5vh]'>
-            jfdj
+    <div id='heroSection' className=''>
+      <div className='flex md:flex-row flex-col-reverse justify-between items-center'>
+        <div id='leftPart' className=' md:w-[50%] md:text-left text-center flex flex-col gap-4 md:items-start items-center'>
+          <div className='flex gap-2'>
+            <div className='bg-[#177771] w-20 h-3 rounded-lg'></div>
+            <div className='bg-[#FF7F40] w-8 h-3 rounded-lg'></div>
+          </div>
+          <div id='nameANDprofession'>
+            <h1 className='md:text-5xl text-4xl'>
+              I'm Adil Ezerouki
+            </h1>
+            <h1 className='md:text-5xl text-4xl'>
+              Ful-Stack Web Developer
+            </h1>
+          </div>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            Exercitationem sapiente rerum aspernatur! Consequuntur in
+            incidunt consequatur aspernatur aperiam molestiae autem rem
+            corporis deleniti, ea facere consectetur blanditiis obcaecati
+            nesciunt harum?</p>
+          <div id='btns' className='flex md:flex-row flex-col gap-4'>
+            <button className='bg-[#177771] text-white px-6 py-3 rounded-full' type='button'>DOWNLOAD CV</button>
+            <button className='border border-[#177771] text-[#177771] px-6 py-3 rounded-full' type='button'>VIEW PORTFOLIO</button>
+          </div>
         </div>
+        <div id='rightPart' className=''>
+          <img src={img} alt="adil picture" className='' />
+        </div>
+      </div>
     </div>
   )
 }
