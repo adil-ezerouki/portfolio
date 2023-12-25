7069f0f800d535f8252d8ba2a1ae05be02f498e1
Adil Ezerouki <adil.ezerouki@gmail.com>
2023-12-26 00:31:15 +0100

    typing effect and changing font-family of the app

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,13 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import img from '../assets/adil1.png'
+import Typed from 'react-typed'
 
 export default function Hero() {
+  const typeTarget = useRef(null);
+  console.log(new Typed)
+
   return (
-    <div id='heroSection' className='bg-yellow-200'>
+    <div id='heroSection' className=''>
       <div className='flex lg:flex-row flex-col-reverse justify-between items-center'>
         <div id='leftPart' className=' lg:w-[50%] lg:text-left text-center flex flex-col gap-4 lg:items-start items-center'>
           <div className='flex gap-2'>
@@ -11,11 +15,24 @@ export default function Hero() {
             <div className='bg-[#FF7F40] w-8 h-3 rounded-lg'></div>
           </div>
           <div id='nameANDprofession'>
-            <h1 className='lg:text-5xl text-4xl'>
-              I'm Adil Ezerouki
-            </h1>
-            <h1 className='lg:text-5xl text-4xl'>
-              Ful-Stack Web Developer
+            <div className='lg:text-5xl text-4xl'>
+              I'm <span className='text-[#FF7F40]'>Adil</span> <span className='text-[#177771]'>Ezerouki</span>
+            </div>
+
+
+            <h1 className='lg:text-5xl text-4xl' ref={typeTarget}>
+            <Typed 
+                strings={[
+                  "Front-end Dev",
+                  "Back-end Dev",
+                  "Full-stack Dev",
+                ]}
+
+                typeSpeed={100}
+                backSpeed={100}
+                loop
+              
+              />
             </h1>
           </div>
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit.
